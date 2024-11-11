4d81fda4274197a6919660e43efdca0d8a914456
zappz88 <aramshaw88@gmail.com>
2024-11-10 20:49:16 -0600

    fully encrypt payload data on request, response
    fully encrypt payload data on request, response

--- common/projects/common/src/lib/service/user/user.service.ts
+++ common/projects/common/src/lib/service/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map, Observable, tap } from 'rxjs';
 import { 
   User,
   Credential,
@@ -31,91 +31,72 @@ export class UserService {
       this.encryptor = EncryptorFactory.getEncryptorByString(this.appjsonconfig.encryptor);
   }
 
-  getUserByCredentials(username: string, password: string) : Observable<User>{
-    if(this.encryptor){
-      const json = new JsonPayload(this.encryptor.encrypt(JSON.stringify({username: username, password: password})));
-      return this.httpClient.post<User>(`${this.uri}/GetUserByCredentials`, json);
-    }
-    else{
-      throw new Error("Encryptor not implemented exception.");
-    }
+  getUserByCredentials(username: string, password: string) : Observable<User> {
+    const json = new JsonPayload(this.stringifyAndEncryptJson({username: username, password: password}));
+    return this.httpClient.post<JsonPayload>(`${this.uri}/GetUserByCredentials`, json)
+           .pipe(map((response) => this.decryptAndParseJson(response.data)));
   }
 
   getUserByCredential(credential: Credential) : Observable<User>{
-    if(this.encryptor){
-      const json = new JsonPayload(this.encryptor.encrypt(JSON.stringify(credential)));
-      return this.httpClient.post<User>(`${this.uri}/GetUserByCredential`, json);
-    }
-    else{
-      throw new Error("Encryptor not implemented exception.");
-    }
+    const json = new JsonPayload(this.stringifyAndEncryptJson(credential));
+    return this.httpClient.post<JsonPayload>(`${this.uri}/GetUserByCredential`, json)
+           .pipe(map((response) => this.decryptAndParseJson(response.data)));
   }
 
   getUserByUserCredential(userCredential: UserCredential) : Observable<User>{
-    if(this.encryptor){
-      const json = new JsonPayload(this.encryptor.encrypt(JSON.stringify(userCredential)));
-      return this.httpClient.post<User>(`${this.uri}/GetUserByCredential`, json);
-    }
-    else{
-      throw new Error("Encryptor not implemented exception.");
-    }
+    const json = new JsonPayload(this.stringifyAndEncryptJson(userCredential));
+    return this.httpClient.post<JsonPayload>(`${this.uri}/GetUserByCredential`, json)
+           .pipe(map((response) => this.decryptAndParseJson(response.data)));
   }
 
-  getUserIDByCredentials(username: string, password: string) : Observable<User>{
-    if(this.encryptor){
-      const json = new JsonPayload(this.encryptor.encrypt(JSON.stringify({username: username, password: password})));
-      return this.httpClient.post<User>(`${this.uri}/GetUserIDByCredentials`, json);
-    }
-    else{
-      throw new Error("Encryptor not implemented exception.");
-    }
+  getUserIDByCredentials(username: string, password: string) : Observable<number>{
+    const json = new JsonPayload(this.stringifyAndEncryptJson({username: username, password: password}));
+    return this.httpClient.post<JsonPayload>(`${this.uri}/GetUserIDByCredentials`, json)
+           .pipe(map((response) => this.decryptAndParseJson(response.data)));
   }
 
   getUserIDByCredential(credential: Credential) : Observable<number>{
-    if(this.encryptor){
-      const json = new JsonPayload(this.encryptor.encrypt(JSON.stringify(credential)));
-      return this.httpClient.post<number>(`${this.uri}/GetUserIDByCredential`, json);
-    }
-    else{
-      throw new Error("Encryptor not implemented exception.");
-    }
+    const json = new JsonPayload(this.stringifyAndEncryptJson(credential));
+    return this.httpClient.post<JsonPayload>(`${this.uri}/GetUserIDByCredential`, json)
+           .pipe(map((response) => this.decryptAndParseJson(response.data)));
   }
 
   getUserIDByUserCredential(userCredential: UserCredential) : Observable<number>{
-    if(this.encryptor){
-      const json = new JsonPayload(this.encryptor.encrypt(JSON.stringify(userCredential)));
-      return this.httpClient.post<number>(`${this.uri}/GetUserIDByUserCredential`, json);
-    }
-    else{
-      throw new Error("Encryptor not implemented exception.");
-    }
+    const json = new JsonPayload(this.stringifyAndEncryptJson(userCredential));
+    return this.httpClient.post<JsonPayload>(`${this.uri}/GetUserIDByUserCredential`, json)
+           .pipe(map((response) => this.decryptAndParseJson(response.data)));
   }
 
   insertUser(user: User) : Observable<User>{
+    const json = new JsonPayload(this.stringifyAndEncryptJson(user));
+    return this.httpClient.post<JsonPayload>(`${this.uri}/InsertUser`, json)
+           .pipe(map((response) => this.decryptAndParseJson(response.data)));
+  }
+
+  updateUser(user: User) : Observable<number>{
+    const json = new JsonPayload(this.stringifyAndEncryptJson(user));
+    return this.httpClient.post<JsonPayload>(`${this.uri}/UpdateUser`, json)
+           .pipe(map((response) => this.decryptAndParseJson(response.data)));
+  }
+
+  deleteUser(user: User) : Observable<number>{
+    const json = new JsonPayload(this.stringifyAndEncryptJson(user));
+    return this.httpClient.post<JsonPayload>(`${this.uri}/DeleteUser`, json)
+           .pipe(map((response) => this.decryptAndParseJson(response.data)));
+  }
+
+  stringifyAndEncryptJson(val: any){
     if(this.encryptor){
-      console.log(this.encryptor.encrypt(JSON.stringify(user)));
-      const json = new JsonPayload(this.encryptor.encrypt(JSON.stringify(user)));
-      return this.httpClient.post<User>(`${this.uri}/InsertUser`, json);
+      return this.encryptor.encrypt(JSON.stringify(val));
     }
     else{
       throw new Error("Encryptor not implemented exception.");
     }
   }
 
-  updateUser(user: User) : Observable<User>{
+  decryptAndParseJson(val: any){
     if(this.encryptor){
-      const json = new JsonPayload(this.encryptor.encrypt(JSON.stringify(user)));
-      return this.httpClient.post<User>(`${this.uri}/UpdateUser`, json);
-    }
-    else{
-      throw new Error("Encryptor not implemented exception.");
-    }
-  }
-
-  deleteUser(user: User) : Observable<User>{
-    if(this.encryptor){
-      const json = new JsonPayload(this.encryptor.encrypt(JSON.stringify(user)));
-      return this.httpClient.post<User>(`${this.uri}/DeleteUser`, json);
+      return JSON.parse(this.encryptor.decrypt(val));
     }
     else{
       throw new Error("Encryptor not implemented exception.");
